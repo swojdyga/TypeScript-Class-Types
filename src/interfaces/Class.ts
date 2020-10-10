bb2153a50482d24f9560ae7aff5da5d8b7d5fbda
Sebastian Wojdyga <wojdyga.sebastian@gmail.com>
2020-10-10 19:07:06 +0200

    Fixing forcing empty array

--- src/interfaces/Class.ts
+++ src/interfaces/Class.ts
@@ -3,7 +3,7 @@ import AbstractClass from "./AbstractClass";
 
 export default interface Class<
     T extends any = any,
-    A extends any[] = [],
+    A extends any[] = any[],
 > extends AbstractClass<T>, Constructor<T, A> {
 
 }
--- src/interfaces/Constructor.ts
+++ src/interfaces/Constructor.ts
@@ -1,6 +1,6 @@
 export default interface Constructor<
     T extends any = any,
-    A extends any[] = [],
+    A extends any[] = any[],
 > {
     new (...args: A): T;
 }
